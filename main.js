47a8d1bb316b105808ed8ce51035d5af6071ef31
Dr3amTw1st <toddfacemyer@ymail.com>
2014-07-31 14:50:49 -0400

    Added the capture feature.
    You can now capture your opponents pieces by landing your last pebble in an empty pit across from your opponents pieces.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var a, b, c, d, e, f, p1, g, h, i, j, k, l, p2;
 var slots = ["a", "b", "c", "d", "e", "f", "p1", "g", "h", "i", "j", "k", "l", "p2"];
-var playerTurn = 1;
+var playerTurn = 1; // There are plans to make this random.
 
 function initialize() {
 	a=4, b=4, c=4, d=4, e=4, f=4, p1=0, g=4, h=4, i=4, j=4, k=4, l=4, p2=0;
@@ -23,12 +23,12 @@ function initialize() {
 }
 
 function updateTurn() {
-		if (playerTurn === 1) {
-			playerTurn = 2;
-		}
-		else {
-			playerTurn = 1;
-		}
+	if (playerTurn === 1) {
+		playerTurn = 2;
+	}
+	else {
+		playerTurn = 1;
+	}
 	
 	document.getElementById("playerTurn").innerHTML = "It is player " + playerTurn + "'s turn.";
 }
@@ -54,7 +54,7 @@ function distribute(clickedSlot, pebbles) {
 				currentSlot = 0;
 			}
 			
-			// This part is kind of complex... basically just place a pebble in the current slot.
+			// Place a pebble in the current slot.
 			document.getElementById(slots[currentSlot]).value = parseInt(document.getElementById(slots[currentSlot]).value) + 1;
 			
 			// Get the last slot (used to check for captures and extra turns)
@@ -63,7 +63,24 @@ function distribute(clickedSlot, pebbles) {
 			}
 		}
 		
-		// Whose turn is it now?
+		// Captures?
+		if (parseInt(document.getElementById(slots[lastSlot]).value) === 1 && checkSlotOwner() && checkAdjacent()) {
+			//alert("Capture!");
+			opponentPebbles = document.getElementById(slots[adjacent]).value;
+			document.getElementById(slots[lastSlot]).value = 0;
+			document.getElementById(slots[adjacent]).value = 0;
+			if (playerTurn === 1) {
+				document.getElementById(slots[6]).value = parseInt(document.getElementById(slots[6]).value) + parseInt(opponentPebbles) + 1;
+			}
+			else if (playerTurn === 2) {
+				document.getElementById(slots[13]).value = parseInt(document.getElementById(slots[13]).value) + parseInt(opponentPebbles) + 1;
+			}
+			else {
+				console.log("Error qd412");
+			}
+		}
+		
+		// Extra Turn?
 		if (playerTurn === 1 && lastSlot === 6) {
 			playerTurn = 1;
 		}
@@ -77,7 +94,6 @@ function distribute(clickedSlot, pebbles) {
 }
 
 function validateMove(clickedSlot, pebbles) {
-
 	if (playerTurn === 1 && (currentSlot === 0 || currentSlot === 1 || currentSlot === 2 || currentSlot === 3 || currentSlot === 4 || currentSlot === 5)) {
 		if (pebbles === 0) {
 			alert("There are no pebbles there!");
@@ -100,5 +116,64 @@ function validateMove(clickedSlot, pebbles) {
 		alert("It's not your turn!");
 		return false;
 	}
+}
 
+function checkAdjacent() {
+	switch (lastSlot) {
+		case 0:
+			adjacent = 12;
+			break;
+		case 1:
+			adjacent = 11;
+			break;
+		case 2:
+			adjacent = 10;
+			break;
+		case 3:
+			adjacent = 9;
+			break;
+		case 4:
+			adjacent = 8;
+			break;
+		case 5:
+			adjacent = 7;
+			break;
+		// Case 6 is Player 1's scoring bank
+		case 7:
+			adjacent = 5;
+			break;
+		case 8:
+			adjacent = 4;
+			break;
+		case 9:
+			adjacent = 3;
+			break;
+		case 10:
+			adjacent = 2;
+			break;
+		case 11:
+			adjacent = 1;
+			break;
+		case 12:
+			adjacent = 0;
+			break;
+		// Case 13 is Player 2's scoring bank
+		default:
+			console.log("Error poq2r");
+	}
+	if (document.getElementById(slots[adjacent]).value > 0) {
+		return true;
+	}
+}
+
+function checkSlotOwner() {
+	if (playerTurn === 1 && (lastSlot === 0 || lastSlot === 1 || lastSlot === 2 || lastSlot === 3 || lastSlot === 4 || lastSlot === 5)) {
+		return true;
+	}
+	else if (playerTurn === 2 && (lastSlot === 7 || lastSlot === 8 || lastSlot === 9 || lastSlot === 10 || lastSlot === 11 || lastSlot === 12)) {
+		return true;
+	}
+	else {
+		return false;
+	}
 }
